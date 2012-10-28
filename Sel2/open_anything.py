48154759abb05a5508286dcf0492e11ad75ccbea
Morphik83 <morphik@morphik-VirtualBox>
2012-10-28 23:18:28 +0100

    simple redirect handler - initial commit

---
+++ Sel2/open_anything.py
@@ -0,0 +1,72 @@
+import urllib2
+import time
+
+class DefaultErrorHandler(urllib2.HTTPDefaultErrorHandler):
+    """this is were description for class goes"""
+        
+    def http_error_default(self, req, fp, code, msg, hdrs):
+        """urllib2 searches for http_error_default when error code 304 is thrown
+
+        in original urllib2.HTTPErrorHandler, method http_error_default throws 
+        exception - this method overwrites this behavior
+        """
+        result = urllib2.HTTPError(req.get_full_url(), code, msg, hdrs, 
+         fp)
+        result.status = code
+        return result
+    
+class SmartRedirectHandler(urllib2.HTTPRedirectHandler):
+    def http_error_301(self, req, fp, code, msg, headers):
+        result = urllib2.HTTPRedirectHandler.http_error_301(self, req, fp, code, msg, headers)
+        #result.URL_1 = [headers[h] for h in headers.keys() if h == 'location']
+        result._301_URL = headers.getheader('location')
+        result._301_STATUS = code
+        result._301_REQUEST_GET_FULL_URL = req.get_full_url()
+        result._301_DATE = headers.getheader('date')
+        result._301_TIMESTAMP = time.strftime()
+        return result
+    
+    def http_error_302(self, req, fp, code, msg, headers):
+        result = urllib2.HTTPRedirectHandler.http_error_302(self, req, fp, code, msg, headers)
+        #result.URL_2 = [headers[h] for h in headers.keys() if h == 'location']
+        result._302_URL = headers.getheader('location')
+        result._302_STATUS = code
+        result._302_REQUEST_GET_FULL_URL = req.get_full_url()
+        result._302_DATE = headers.getheader('date')
+        result._302_TIMESTAMP = time.strftime()
+        return result
+
+if __name__ == '__main__':
+    import pprint
+    import re
+    
+    srh = SmartRedirectHandler()
+    #handler = urllib2.HTTPHandler()
+    #handler.set_http_debuglevel(1)
+    opener = urllib2.build_opener(srh)
+    opener.handle_open['http'][0].set_http_debuglevel(1)
+    request = urllib2.Request('http://www.volvopenta.com')
+    f = opener.open(request)
+    
+    #pprint.pprint(f.__dict__)
+    myList = [(item,f.__dict__[item]) for item in f.__dict__.keys()\
+               if re.match(r'^_[^_]|code|url|Date', item)]
+    pprint.pprint(myList)
+    #===========================================================================
+    # print 'from_REQ_GET_FULL_URL_1: ', f.REQUEST_GET_FULL_URL_1
+    # print 'to_URL_1: ',f.URL_1
+    # print 'STATUS_1: ', f.STATUS_1
+    # print 'STATUS_1: ', f.STATUS
+    # 
+    # print "*"*30
+    # print 'from_REQ_GET_FULL_URL_2: ', f.REQUEST_GET_FULL_URL_2
+    # print 'to_URL_2: ',f.URL_2
+    # print 'STATUS_2: ', f.STATUS_2
+    # print 'STATUS_2: ', f.STATUS
+    #===========================================================================
+    
+    
+    #r'^_[^_]|code|url' ---> OK: _302_URL, NOTOK: __302_URL'
+    #match words that starts from only one underscore - second char 
+    #cannot be underscore [^_]
+        
