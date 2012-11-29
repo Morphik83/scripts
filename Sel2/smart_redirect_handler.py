8798e28d4a2afb67fe47a4b9daecd4843d652fe0
Morphik83 <Morphikx@poczta.onet.pl>
2012-11-30 00:36:06 +0100

    redirect sys.stdout to logFile
    Signed-off-by: Morphik83 <Morphikx@poczta.onet.pl>

--- Sel2/smart_redirect_handler.py
+++ Sel2/smart_redirect_handler.py
@@ -1,5 +1,3 @@
-#>>> this is just checking for the new branch
-
 import urllib2
 
 class DefaultErrorHandler(urllib2.HTTPDefaultErrorHandler):
@@ -17,38 +15,53 @@ class DefaultErrorHandler(urllib2.HTTPDefaultErrorHandler):
         return result
     
 class SmartRedirectHandler(urllib2.HTTPRedirectHandler):
+    
     def http_error_301(self, req, fp, code, msg, headers):
         result = urllib2.HTTPRedirectHandler.http_error_301(self, req, fp, code, msg, headers)
         #result.URL_1 = [headers[h] for h in headers.keys() if h == 'location']
+        result._301_FROM_REQUEST_GET_FULL_URL = req.get_full_url()
         result._301_TO_URL = headers.getheader('location')
         result._301_STATUS = code
-        result._301_FROM_REQUEST_GET_FULL_URL = req.get_full_url()
         result._301_DATE = headers.getheader('date')
         return result
     
     def http_error_302(self, req, fp, code, msg, headers):
         result = urllib2.HTTPRedirectHandler.http_error_302(self, req, fp, code, msg, headers)
         #result.URL_2 = [headers[h] for h in headers.keys() if h == 'location']
-        result._302_TO_URL = headers.getheader('location')
-        result._302_STATUS = code
         result._302_FROM_REQUEST_GET_FULL_URL = req.get_full_url()
+        result._302_TO_URL = headers.getheader  ('location')
+        result._302_STATUS = code
         result._302_DATE = headers.getheader('date')
-        #result._302_TIMESTAMP = time.strftime("%b %d %Y %H:%M:%S")
         return result
+    
+    def writeTo_Log(self, a):
+        with open("D:\\tmp\\redirect.log", "a+") as f_obj:
+            f_obj.write(str(a))
+        
 
 if __name__ == '__main__':
+    """"
+    ToDo:
+    -unify all the returned headers (the same name!)
+    -add logging 
+    -add graph to make analyzing results easier 
+    """
+    
     import pprint
     import re
-    
+    import sys
+    sys.stdout = open("D:\\tmp\\redirect.log", "a+")
     srh = SmartRedirectHandler()
     #handler = urllib2.HTTPHandler()
     #handler.set_http_debuglevel(1)
     opener = urllib2.build_opener(srh)
-    #opener.handle_open['http'][0].set_http_debuglevel(1)
+    opener.handle_open['http'][0].set_http_debuglevel(1)
     request = urllib2.Request('http://www.volvopenta.com')
     f = opener.open(request)
-        
+    
+    
     pprint.pprint(f.__dict__)
+    
 
     
     #===========================================================================
