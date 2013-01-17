bdcaeec5be4d9e1fd92bd24aa17d0af720123fa6
Morphik83 <maciej.balazy@consultant.volvo.com>
2013-01-17 14:46:38 +0100

    .

--- web_crawler/my_crawler.py
+++ web_crawler/my_crawler.py
@@ -30,16 +30,16 @@ In that way, urls_to_follow should start to shrink eventually
 
 
 #host_url = 'http://online.renault-trucks-qa.volvo.com'
-host_url = 'http://volvogroup-qa.volvo.com/group/global/en-gb'
-start_url = 'http://volvogroup-qa.volvo.com/group/global/en-gb/Pages/group_home.aspx'
+host_url = 'http://volvogroup.com/group/global/en-gb'
+start_url = 'http://volvogroup.com/group/global/en-gb/Pages/group_home.aspx'
 #start_url = 'http://online.renault-trucks-qa.volvo.com'
 headers = ('User-Agent','Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0; .NET CLR 2.0.50727; .NET CLR 1.1.4322; .NET CLR 3.0.4506.2152; .NET CLR 3.5.30729; .NET4.0C; .NET4.0E; MS-RTC LM 8; InfoPath.3')
 
 crawler_log_path = os.path.join(os.getcwd(),'logs')
 crawler_log = os.path.join(crawler_log_path, 'CRAWLER.log')
 
-username = 'xv02466'
-passwd = 'welcome2'
+username = '****'
+passwd = '******'
 
 b = mechanize.Browser()
 b.set_debug_http(True)
